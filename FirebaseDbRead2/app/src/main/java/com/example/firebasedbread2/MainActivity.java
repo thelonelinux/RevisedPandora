2c311c62465c63406a79ab3717abda52f2e05800
Vicky <vickythetechguy@gmail.com>
2019-06-26 10:27:24 +0530

    FirbaseAuth

--- FirebaseDbRead2/app/src/main/java/com/example/firebasedbread2/MainActivity.java
+++ FirebaseDbRead2/app/src/main/java/com/example/firebasedbread2/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.firebasedbread2;
 
 import android.content.Intent;
-import android.content.Intent;
-import android.os.Parcelable;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,8 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -22,9 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements ChildEventListener {
